886e3af249d2ffd64d411f5e15a00def90e8f9b5
2ndpot <aosirojun@gmail.com>
2026-08-27 15:27:40 +0900

    Update youtube-skipper.js

--- youtube-skipper.js
+++ youtube-skipper.js
@@ -43,9 +43,9 @@
         </div>
       </div>
       <div class="yts-controls">
-        <button class="yts-btn yts-btn-primary" id="yts-btn-preset">プリセットに戻す</button>
-        <button class="yts-btn" id="yts-btn-all1">全曲1回（標準）</button>
-        <button class="yts-btn yts-btn-danger" id="yts-btn-stop">緊急停止</button>
+        <button class="yts-btn yts-btn-primary" id="yts-btn-preset">偏愛(プリセット)</button>
+        <button class="yts-btn" id="yts-btn-all1">博愛(全曲1回）</button>
+        <button class="yts-btn yts-btn-danger" id="yts-btn-stop">停止</button>
       </div>
       <table class="yts-table">
         <thead>
